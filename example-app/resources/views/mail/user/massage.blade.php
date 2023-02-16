afcbb54a329c9230886e5d5e69eafdb15336de03
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-02-16 18:04:25 +0300

    Поиск у пользователя

--- example-app/resources/views/mail/user/massage.blade.php
+++ example-app/resources/views/mail/user/massage.blade.php
@@ -1,5 +1,21 @@
 <x-mail::message>
-    Завдравтвуйте, вас добавили в базу данных пользователей, ваш пароль:
-    <br><br>
-{{$password}}
+    # branco.rus
+
+    Завдравтвуйте, вас добавили в базу данных пользователей
+<html>
+<table class="panel" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td class="panel-content">
+            <table width="100%" cellpadding="0" cellspacing="0" role="presentation">
+                <tr>
+                    <td class="panel-item">
+                        ваш пароль: {{$password}}
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+</html>
 </x-mail::message>
