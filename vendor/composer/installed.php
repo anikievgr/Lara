c8fd039be1622fd45cb2cec539b514fb17b06c57
anikievgr <anikiev.gleb.2005@yandex.ru>
2023-02-28 17:22:38 +0300

    now

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '24b5b1c61029cd9bce7a1430d4629aee9e014970',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '24b5b1c61029cd9bce7a1430d4629aee9e014970',
+            'dev_requirement' => false,
+        ),
+    ),
+);
